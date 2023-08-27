95d2c8d9d2d066f76771cad1c271bc608f53fa72
AR(++X)AN <59042198+aryan-bansal@users.noreply.github.com>
2023-08-27 23:48:24 +0530

    removed TGA format

--- main.c
+++ main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdint.h>
 
-typedef struct {
-    uint8_t IDLength;
-    uint8_t ColorMapType;
-    uint8_t ImageType;
-    uint16_t ColorMapStart;
-    uint16_t ColorMapLength;
-    uint8_t colorMapDepth;
-    uint16_t XOrigin;
-    uint16_t YOrigin;
-    uint16_t Width;
-    uint16_t Height;
-    uint8_t BitsPerPixel;
-    uint8_t ImageDescriptor;
-} TGAHeader;
+// typedef struct {
+//     uint8_t IDLength;
+//     uint8_t ColorMapType;
+//     uint8_t ImageType;
+//     uint16_t ColorMapStart;
+//     uint16_t ColorMapLength;
+//     uint8_t colorMapDepth;
+//     uint16_t XOrigin;
+//     uint16_t YOrigin;
+//     uint16_t Width;
+//     uint16_t Height;
+//     uint8_t BitsPerPixel;
+//     uint8_t ImageDescriptor;
+// } TGAHeader;
 
 typedef struct {
     char Format[3];
@@ -26,7 +26,7 @@ typedef struct {
 int main(){
     char filename[100];
 
-    printf("Enter filename: ");
+    printf("Enter PPM filename: ");
     scanf("%s", filename);
 
     FILE *file = fopen(filename, "rb");
@@ -38,15 +38,15 @@ int main(){
     uint8_t ImageType;
     fread(&ImageType, sizeof(uint8_t), 1, file);
 
-    if (ImageType == 1 || ImageType == 2 || ImageType == 3 || ImageType == 9 || ImageType == 10 || ImageType == 11){
-        TGAHeader tgaHeader;
-        fread(&tgaHeader, sizeof(TGAHeader), 1, file);
+    // if (ImageType == 1 || ImageType == 2 || ImageType == 3 || ImageType == 9 || ImageType == 10 || ImageType == 11){
+    //     TGAHeader tgaHeader;
+    //     fread(&tgaHeader, sizeof(TGAHeader), 1, file);
 
-        printf("Image Width: %d\n", tgaHeader.Width);
-        printf("Image Height: %d\n", tgaHeader.Height);
-        printf("Image Size: %d bytes\n", tgaHeader.Width * tgaHeader.Height * tgaHeader.BitsPerPixel / 8);
-    }
-    else if (ImageType == 'P' && fgetc(file) == '6'){
+    //     printf("Image Width: %d\n", tgaHeader.Width);
+    //     printf("Image Height: %d\n", tgaHeader.Height);
+    //     printf("Image Size: %d bytes\n", tgaHeader.Width * tgaHeader.Height * tgaHeader.BitsPerPixel / 8);
+    // }
+    if (ImageType == 'P' && fgetc(file) == '6'){
         PPMHeader ppmHeader;
         fscanf(file, "%d %d\n%d\n", &ppmHeader.Width, &ppmHeader.Height, &ppmHeader.MaxColorValue);
 
